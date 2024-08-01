849f56a8c146c5c4f5f70cca7638d33d96bb85d8
ThaVip <sheriffmusazarami@gmail.com>
2024-08-01 16:54:36 -0700

    first four

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module provides a simple function to add two numbers.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two numbers and returns the result.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
+
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to return the floor of a float.
+"""
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the float n.
+
+    Args:
+        n (float): The float number to floor.
+
+    Returns:
+        int: The floor of the float.
+    """
+    return math.floor(n)
+
